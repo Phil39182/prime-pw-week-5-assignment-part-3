b737c5ac584ebe9538cb6ed98ee557412c244c5b
Josh Snyder <joshrsnyder@gmail.com>
2022-04-29 18:17:19 -0500

    finished addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,19 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection (title,artist,yearPublished) {
+    const album = {
+        title: title,
+        artist: artist,
+        year: yearPublished,
+    }
+    collection.push(album);
+    return album;
+}//end addToCollection
+
+//test addToCollection
+
+console.log('running addToCollection');
+console.log(addToCollection('Give it All', 'Rise Against', 2011));
+console.log(`Show collection ${JSON.stringify(collection)} `);
