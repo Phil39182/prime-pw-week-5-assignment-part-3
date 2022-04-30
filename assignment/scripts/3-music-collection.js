4fa0186921bb24f486be69cebee1d3016ec35b7b
Josh Snyder <joshrsnyder@gmail.com>
2022-04-30 12:24:56 -0500

    finished showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,6 +14,35 @@ function addToCollection (title,artist,yearPublished) {
 
 //test addToCollection
 
-console.log('running addToCollection');
-console.log(addToCollection('Give it All', 'Rise Against', 2011));
-console.log(`Show collection ${JSON.stringify(collection)} `);
+console.log(addToCollection('Siren Song of the Counter Culture', 'Rise Against', '2011'));
+console.log(addToCollection('Tenacious D', 'Tenacious D', '2001'));
+console.log(addToCollection('Stadium Arcadium', 'Red Hot Chili Peppers', '2006'));
+console.log(addToCollection('Appeal to Reson', 'Rise Against', '2008'));
+console.log(addToCollection('The Question', 'Emery', '2005'));
+console.log(addToCollection('Fight With Tools', 'Flobots', '2008'));
+
+console.log(collection);
+
+//function to show number of albums in collection and list each album
+function showCollection( collection ) {
+    console.log('Number of Albums in Collection: ' + collection.length);
+    for( let i = 0; i < collection.length; i++) {
+        console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].year} `);
+    }
+}//end showCollection
+
+console.log(`Running showCollection`);
+console.log(`${showCollection(collection)}`);
+
+function findByArtist( artistSearch ) {
+    let results = [];
+    for( let i = 0; i < collection.length; i++) {
+        if ( collection[i].artist === artistSearch ) {
+            //console.log('Artist Found')
+            //console.log(collection[i]);
+            results.push(collection[i]);
+        }
+    }
+    return results;
+}//end findByArtist
+
