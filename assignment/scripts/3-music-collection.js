a56e01223b40df8db424471bafedc14b3aa9aa4f
Josh Snyder <joshrsnyder@gmail.com>
2022-05-01 11:47:16 -0500

    added tracks

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,11 +2,19 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection (title,artist,yearPublished) {
+function addToCollection (
+    title,artist,yearPublished,
+    song0,song0Duration,
+    song1,song1Duration,
+    song2,song2Duration) {
     const album = {
         title: title,
         artist: artist,
         year: yearPublished,
+        tracks: [
+            song0, song0Duration,
+            song1, song1Duration,
+            song2, song2Duration ]
     }
     collection.push(album);
     return album;
@@ -14,12 +22,18 @@ function addToCollection (title,artist,yearPublished) {
 
 //test addToCollection
 
-console.log(addToCollection('Siren Song of the Counter Culture', 'Rise Against', '2011'));
-console.log(addToCollection('Tenacious D', 'Tenacious D', '2001'));
-console.log(addToCollection('Stadium Arcadium', 'Red Hot Chili Peppers', '2006'));
-console.log(addToCollection('Appeal to Reson', 'Rise Against', '2008'));
-console.log(addToCollection('The Question', 'Emery', '2005'));
-console.log(addToCollection('Fight With Tools', 'Flobots', '2008'));
+console.log(addToCollection('Siren Song of the Counter Culture', 'Rise Against', '2011',
+            'Paper Wings','3:43','Give it All','2:50','Swing Life Away','3:20'));
+console.log(addToCollection('The Pick of Destiny', 'Tenacious D', '2006',
+            'Kickapoo','4:14','Classico','0:58','Master Exploder','2:24'));
+console.log(addToCollection('Stadium Arcadium', 'Red Hot Chili Peppers', '2006',
+            'Dani California','4:42','Snow (Hey Oh)','5:34','Warlocks','3:25'));
+console.log(addToCollection('Appeal to Reason', 'Rise Against', '2008',
+            'Collapse','3:19','Re-Education','3:42','Savior','Savior','4:02'));
+console.log(addToCollection('The Question', 'Emery', '2005',
+            'Stydying Politics','3:30','Listening to Freddie Mercury','2:42','The Weakest','4:04'));
+console.log(addToCollection('Fight With Tools', 'Flobots', '2008',
+            'Stand Up','4:39','Handlebars','3:26','Rise','4:13'));
 
 console.log(collection);
 
@@ -28,6 +42,9 @@ function showCollection( collection ) {
     console.log('Number of Albums in Collection: ' + collection.length);
     for( let i = 0; i < collection.length; i++) {
         console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].year} `);
+        console.log(` ${collection[i].tracks[0]}: ${collection[i].tracks[1]}`);
+        console.log(` ${collection[i].tracks[2]}: ${collection[i].tracks[3]}`);
+        console.log(` ${collection[i].tracks[4]}: ${collection[i].tracks[5]}`);
     }
 }//end showCollection
 
@@ -37,15 +54,13 @@ console.log(`*****  Testing showCollection  *****`)
 console.log(`${showCollection(collection)}`);
 
 function findByArtist( artist ) {
-    let results = [];
+    let findbyArtistresults = [];
     for( let i = 0; i < collection.length; i++) {
         if ( collection[i].artist === artist ) {
-            //console.log('Artist Found')
-            //console.log(collection[i]);
-            results.push(collection[i]);
+            findbyArtistresults.push(collection[i]);
         }
     }
-    return results;
+    return findbyArtistresults;
 }//end findByArtist
 
 //Testing findByArtist
@@ -59,3 +74,9 @@ console.log( findByArtist('Flobots') );
 
 //end test of findByArtist
 
+function search ( criteria ) {
+    let searchResults = [];
+    for( let i = 0; i < collection.length; i++) {
+
+    }
+}//end search function
